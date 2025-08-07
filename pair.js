fed8b31f540f6cf3f0cb9a013256dd70842e75c7
royalmatch594-stack <royalmatch594@gmail.com>
2025-08-07 20:19:36 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -75,11 +75,11 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "malvin~" + string_session;
+                        let md = "NIMA~" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
-                        let desc = `*Hey there, MALVIN-XD User!* 👋🏻
+                        let desc = `*Hey there, NIMA User!* 👋🏻
 
-Thanks for using *MALVIN-XD* — your session has been successfully created!
+Thanks for using *NIMA* — your session has been successfully created!
 
 🔐 *Session ID:* Sent above  
 ⚠️ *Keep it safe!* Do NOT share this ID with anyone.
@@ -87,24 +87,24 @@ Thanks for using *MALVIN-XD* — your session has been successfully created!
 ——————
 
 *✅ Stay Updated:*  
-Join our official WhatsApp Channel:  
-https://whatsapp.com/channel/0029VbA6MSYJUM2TVOzCSb2A
+Join our official WhatsApp group:  
+https://chat.whatsapp.com/HLBP338VvUC0ms5NqCkSSO?mode=ac_t
 
 *💻 Source Code:*  
 Fork & explore the project on GitHub:  
-https://github.com/XdKing2/MALVIN-XD
+https://github.com/Nimeshamadhushan/nima-v4
 
 ——————
 
-> *© Powered by Malvin King*
+> *© Powered by nima*
 Stay cool and hack smart. ✌🏻`; 
                         await sock.sendMessage(sock.user.id, {
 text: desc,
 contextInfo: {
 externalAdReply: {
 title: "ᴍᴀʟᴠɪɴ-xᴅ",
-thumbnailUrl: "https://files.catbox.moe/bqs70b.jpg",
-sourceUrl: "https://whatsapp.com/channel/0029VbA6MSYJUM2TVOzCSb2A",
+thumbnailUrl: "https://files.catbox.moe/w9lv7j.jpg",
+sourceUrl: "https://chat.whatsapp.com/HLBP338VvUC0ms5NqCkSSO?mode=ac_t",
 mediaType: 1,
 renderLargerThumbnail: true
 }  
@@ -123,16 +123,16 @@ Thanks for using *MALVIN-XD* — your session has been successfully created!
 ——————
 
 *✅ Stay Updated:*  
-Join our official WhatsApp Channel:  
-https://whatsapp.com/channel/0029VbA6MSYJUM2TVOzCSb2A
+Join our official WhatsApp group:  
+https://chat.whatsapp.com/HLBP338VvUC0ms5NqCkSSO?mode=ac_t
 
 *💻 Source Code:*  
 Fork & explore the project on GitHub:  
-https://github.com/XdKing2/MALVIN-XD
+https://github.com/Nimeshamadhushan/nima-v4
 
 ——————
 
-> *© Powered by Malvin King*
+> *© Powered by nima*
 Stay cool and hack smart. ✌🏻`;
                             await sock.sendMessage(sock.user.id, {
 text: desc,
